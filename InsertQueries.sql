711633128eda5bc1db6491217d656e48e2cf5c67
Alexander Ivanov <alexander.iv1998@gmail.com>
2019-07-07 00:01:21 +0300

    add update project and sql

---
+++ InsertQueries.sql
@@ -0,0 +1,23 @@
+INSERT INTO pizzas(pizza_name, size, ingredients, price)
+VALUES('BBQ CHICKEN', 'S', 'BBQ SAUCE, MOZZARELLA, CHICKEN BREAST, RED ONION, PEPPERS', 9),
+('BBQ CHICKEN', 'M', 'BBQ SAUCE, MOZZARELLA, CHICKEN BREAST, RED ONION, PEPPERS', 12),
+('MEATBALL', 'S', 'MOZZARELLA, RICOTTA, MEATBALLS, PEPPERS, RED ONION, MICRO BASIL', 10);
+
+INSERT INTO salads(salad_name, ingredients, price)
+VALUES('GARDEN SALAD', 'Romaine, iceberg, red leaf lettuce, Roma tomatoes, cucumbers, red onions, carrots & croutons', 6.50),
+('CAESAR SALAD', 'Chopped romaine tossed in Caesar dressing, croutons & freshly grated Parmesan cheese', 5),
+('SANTA BARBARA COBB', 'Crisp shredded iceberg & romaine lettuce, tomatoes, avocado, blue cheese, crisp bacon & egg', 5.50);
+
+INSERT INTO drinks(drink_type, brand, quantity, price)
+VALUES('BEER', 'Kamenitza', 500, 3),
+('WINE', 'Rose', 250, 4),
+('WATER', 'Devin', 500, 3),
+('alcohol-free', 'Coca-Cola', 1000, 2.50);
+
+INSERT INTO users(username, password, phone, email)
+VALUES('alexander98', 'userPassword1', 0894089756, 'alexander452@gmail.com'),
+('ivan_petrov7', 'userPassword2', 0897568948, 'ivan7_petrov@gmail.com');
+
+INSERT INTO admins(adminName, password, phone, email)
+VALUES('TodorKocev8', 'adminPassword1', 0886269718, 'todor78Kocev@gmail.com'),
+('Georgi96Kulev', 'adminPassword2', 0897586147, 'kulev_georgi79@gmail.com');
